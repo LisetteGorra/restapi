f35e05bc654d3e067e04e05169d39083d8ff3ed7
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-07 15:58:06 +0000

    Creating an express app using app.listen, process.env.port

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 // set up express app
 const app = express();
 
-// initialize routes
-app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
