af5808cf7c6871d6c4cb469b5983363eb6328c93
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-02 19:00:02 +0000

    Resolving geo near code

--- routes/api.js
+++ routes/api.js
@@ -1,75 +1,52 @@
-// #1
 const express = require('express');
 const router = express.Router();
-const Ninja = require('../models/ninja');
+const Ninja = require('../models/ninjas');
 
-// get a list of ninjas from db 
-router.get('/ninjas', function(req, res, next){
-  
-    // Ninja.find({}).then(function(ninjas){
-    //     res.send(ninjas);
-    // });
-  Ninja.geoNear(
-      {type: "Point", coordinates: [parseFloat(req.query.lng), parseFloat(req.quer.lat)]}
-      { maxDistance: 100000, spherical: true }
-      ).then(function(ninjas){
-      res.send(ninjas);
-      });
+// GET request
+router.get("/ninjas", function(req, res, next) {
+  Ninja.aggregate([{
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      includeLocs: "dist.location",
+      maxDistance: 100000,
+      spherical: true
+    }
+  }])
+    .then(function(ninja) {
+      res.send(ninja);
+    })
+    .catch(next);
 });
 
-// ADD a new ninja to the db 
-router.post('/ninjas', function(req, res, next){
-  Ninja.create(req.body).then(function(ninja){
-    res.send(ninja);
+
+// POST request | add a new ninja to the db
+router.post('/ninjas', function(req, res, next) {
+    Ninja.create(req.body).then(function(ninja) {
+        res.send(ninja);
     }).catch(next);
-  });
-
-//update a ninja in the db
-router.put('/ninjas/:id', function(req, res, next){
-    Ninja.findByIdAndUpdate({_id:req.params.id}),req.body).then(function(){
-    Ninja.findOne({_id:req.params.id}).then(function(ninja){
-    res.send(ninja);
-         });
-     });
 });
 
-//delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res, next){
-    Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
-            
+// UPDATE (put) request to put a new record in the db/collection
+router.put('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndUpdate({_id:req.params.id}, req.body).then(function() {
+        Ninja.findOne({_id:req.params.id}).then(function(ninja) {
+           res.send(ninja);  
+        });
+        
+    });
+   
+});
+
+// DELETE request
+router.delete('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja) {
         res.send(ninja);
     });
 });
 
+
 module.exports = router;
-
-
-
-/////Router('./routes/api')
-
-
-// const router = express.Router();
-
-// // get a list of ninjas from the db
-// router.get('/ninjas', function(req, res){
-//     res.send({type: 'GET'});
-// });
-
-// // add a new ninja to the db
-// router.post('/ninjas', function(req, res){
-//     res.send({type: 'POST'});
-// });
-
-// // update a ninja in the db
-// router.put('/ninjas/:id', function(req, res){
-//     res.send({type: 'PUT'});
-// });
-
-// // delete a ninja from the db
-// router.delete('/ninjas/:id', function(req, res){
-//     res.send({type: 'DELETE'});
-// });
-
-// module.exports = router;
-
-//#1
